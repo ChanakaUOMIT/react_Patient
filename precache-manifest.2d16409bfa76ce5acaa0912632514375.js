b3b7ee4af4fa74913366ceead87fb712c6adcc7f
Chanaka Sampath Wickramanayaka <chanakauomfit@gmail.com>
2018-11-02 02:48:34 +0530

    Updates

---
+++ precache-manifest.2d16409bfa76ce5acaa0912632514375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f2ad9512c7ad4ea794d3a5d6adbd69e",
+    "url": "/react_Patient/static/media/spinner.2f2ad951.gif"
+  },
+  {
+    "revision": "fcb6e3fdfc612f0eaf7a",
+    "url": "/react_Patient/static/js/runtime~main.fcb6e3fd.js"
+  },
+  {
+    "revision": "3025405057daa0e9b01b",
+    "url": "/react_Patient/static/js/main.30254050.chunk.js"
+  },
+  {
+    "revision": "a1ca2431a052dafdc4d3",
+    "url": "/react_Patient/static/js/1.a1ca2431.chunk.js"
+  },
+  {
+    "revision": "3025405057daa0e9b01b",
+    "url": "/react_Patient/static/css/main.d197a115.chunk.css"
+  },
+  {
+    "revision": "ba30e7f78a8a35f8167dd7e5da374afd",
+    "url": "/react_Patient/index.html"
+  }
+];
